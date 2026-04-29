e77684045903d7983b4a910571870df85a8709f5
hokb <h.kutschbach@ilnumerics.net>
2026-04-29 12:14:39 +0200

    more cleaning for CD

--- ILNumerics.ONAL/Properties/AssemblyInfo.cs
+++ ILNumerics.ONAL/Properties/AssemblyInfo.cs
@@ -19,14 +19,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Resources;
-using System.Security;
 
 [assembly: ComVisible(false)]
-[assembly: Guid("92f76232-2420-430d-af30-3d66a3c1da64")]
-
-[assembly: AllowPartiallyTrustedCallers(PartialTrustVisibilityLevel = PartialTrustVisibilityLevel.VisibleToAllHosts)]
-[assembly: SecurityRules(SecurityRuleSet.Level2, SkipVerificationInFullTrust = true)]
+[assembly: Guid("A8D97E07-015D-429D-B7A1-986822DB8833")]
